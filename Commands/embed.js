aef2516b2322dfa934cf3008de9a99104a24cc00
Ishitiaq Naser Uthsho <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-26 17:47:54 +0600

    Update embed.js

--- Commands/embed.js
+++ Commands/embed.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const config = require("../Configuration/config.json");
 
 module.exports = async(bot, msg, args) => {
-    if(!msg.member.roles.some(r=>["Staff"].includes(r.name)) )
-    return msg.channel.send({embed: {
+  if(!message.member.hasPermission('ADMINISTRATOR')) {
+      return msg.channel.send({embed: {
         description: `:no_enrty_sign: ${msg.author.username}, you don't have the permission to run this command!`,
         color: 0xff00,
         footer: `Contact your server admin if you think this is wrong`        
