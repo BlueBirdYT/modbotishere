23a2faa18947154d716d356771924c93b753d796
Ishitiaq Naser Uthsho <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-26 23:07:31 +0600

    Update status.js

--- Commands/status.js
+++ Commands/status.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-exports.run = async (bot, message, args) => {
+exports.run = async (bot, msg, args) => {
 
 /*  const list = {
     play: "PLAYING", 
@@ -9,14 +9,14 @@ exports.run = async (bot, message, args) => {
     watch: "WATCHING"
   }; */
 let stat = args.slice(2).join(" ");
-if(!stat) return message.channel.send("Usage: ?status <playing, listening, streaming, watching> <what are you playing or listening or watching or streaming>");
+if(!stat) return msg.channel.send("Usage: ?status <playing, listening, streaming, watching> <what are you playing or listening or watching or streaming>");
 let type = args[1];
-if(!type) return message.channel.send("Usage: ?status <playing, listening, streaming, watching> <what are you playing or listening or watching or streaming>");
+if(!type) return msg.channel.send("Usage: ?status <playing, listening, streaming, watching> <what are you playing or listening or watching or streaming>");
 let status = args[0];
-if(!status) return message.channel.send("What do you want your bot to be");
+if(!status) return msg.channel.send("What do you want your bot to be");
 bot.user.setActivity(stat, {
   type: type
 });
 bot.user.setStatus(`${status}`);
-message.channel.send(`Presence has been set to ${type} ${stat}`);
+msg.channel.send(`Presence has been set to ${type} ${stat}`);
 }
