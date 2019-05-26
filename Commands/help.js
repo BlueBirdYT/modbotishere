588b5153b2f8f5fb6a80a5e7c8f89d73b0fd986c
Ishitiaq Naser Uthsho <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-26 18:10:45 +0600

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -2,23 +2,7 @@ const Discord = require("discord.js");
 const config = require("../Configuration/config.json");
 
 module.exports = async(bot, msg, args) => {
-    if(!msg.member.roles.some(r=>["Staff"].includes(r.name)) ) {
-        const embed = new Discord.RichEmbed()
-        .setAuthor(bot.user.username, bot.user.avatarURL)
-        .setTitle(`__**HELP**__`)
-        .addField(`ping`, `Ping the bot`, false)
-        .addField(`about`, `About TTGProtect`, false)
-        .addField(`serverinfo`, `Info about the server`, false)
-        .addField(`debug`, `Info about the Host and the bot`, false)
-        .addField(`stats`, `View some statistics about TTGProtect`, false)
-        .addField(`uptime`, `How long has the bot been up?`, false)
-        .addField(`profile`, `About your user profile`, false)
-        .setFooter(`You are not seeing all commands as you are not Staff in this server`)
-        .setColor(0x157f87)
-    
-    msg.author.send(embed)
-    msg.channel.send(`:mailbox: ${msg.author.username}, Check your DM's`)
-    } else {
+    if(!message.member.hasPermission('ADMINISTRATOR')) {
         const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setTitle(`__**HELP**__`)
@@ -36,6 +20,21 @@ module.exports = async(bot, msg, args) => {
         .setFooter(`You are viewing all commands`)
         .setColor(0x157f87)
 
+        msg.author.send(embed)
+        msg.channel.send(`:mailbox: ${msg.author.username}, Check your DM's`)        
+    } else {
+        const embed = new Discord.RichEmbed()
+        .setAuthor(bot.user.username, bot.user.avatarURL)
+        .setTitle(`__**HELP**__`)
+        .addField(`ping`, `Ping the bot`, false)
+        .addField(`about`, `About TTGProtect`, false)
+        .addField(`serverinfo`, `Info about the server`, false)
+        .addField(`debug`, `Info about the Host and the bot`, false)
+        .addField(`stats`, `View some statistics about TTGProtect`, false)
+        .addField(`uptime`, `How long has the bot been up?`, false)
+        .addField(`profile`, `About your user profile`, false)
+        .setFooter(`You are not seeing all commands as you are not Staff in this server`)
+        .setColor(0x157f87) 
         msg.author.send(embed)
         msg.channel.send(`:mailbox: ${msg.author.username}, Check your DM's`)
     }
