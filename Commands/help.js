7f5435a0f5ec4ecdeaea8763658370a82157dfea
Ishitiaq Naser Uthsho <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-26 18:32:38 +0600

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const config = require("../Configuration/config.json");
 
 module.exports = async(bot, msg, args) => {
-    if(!message.member.hasPermission('ADMINISTRATOR')) {
+    if(!msg.member.hasPermission('ADMINISTRATOR')) {
         const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setTitle(`__**HELP**__`)
@@ -19,7 +19,24 @@ module.exports = async(bot, msg, args) => {
         .addField(`embed`, `Make an embed to get yourself heard`)
         .setFooter(`You are viewing all commands`)
         .setColor(0x157f87)
-
+ if(!msg.member.hasPermission('View_Audit_log')) {
+        const embed = new Discord.RichEmbed()
+        .setAuthor(bot.user.username, bot.user.avatarURL)
+        .setTitle(`__**HELP**__`)
+        .addField(`ping`, `Ping the bot`, false)
+        .addField(`about`, `About TTGProtect`, false)
+        .addField(`serverinfo`, `Info about the server`, false)
+        .addField(`stats`, `View some statistics about TTGProtect`, false)
+        .addField(`debug`, `Info about the Host and the bot`, false)
+        .addField(`uptime`, `How long has the bot been up?`, false)
+        .addField(`profile`, `About your user profile`, false)
+        .addField(`warn`, `Warn someone about something`, false)
+        .addField(`kick`, `Kick the mentioned user`, false)
+        .addField(`ban`, `Ban the mentioned user`, false)
+        .addField(`embed`, `Make an embed to get yourself heard`)
+        .setFooter(`You are viewing all commands`)
+        .setColor(0x157f87)
+        
         msg.author.send(embed)
         msg.channel.send(`:mailbox: ${msg.author.username}, Check your DM's`)        
     } else {
